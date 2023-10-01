fe565d0b01061186579c25afe3a9164464c6c2b6
VRomanenko04 <vovasan2004@gmail.com>
2023-10-01 20:24:38 +0300

    Перенастроил firebase, сделал контроль LoginForm

--- src/components/simple/LoginForm/LoginForm.tsx
+++ src/components/simple/LoginForm/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './LoginForm.module.scss';
 
 interface ILogin {
@@ -5,17 +6,57 @@ interface ILogin {
 }
 
 const LoginForm = ({ handleChangeChoose }: ILogin) => {
+    const [userLogin, setUserLogin] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserLogin({
+            ...userLogin,
+            email: e.target.value
+        });
+    }
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserLogin({
+            ...userLogin,
+            password: e.target.value
+        });
+    }
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(userLogin);
+        setUserLogin({
+            email: '',
+            password: ''
+        })
+    }
+
     return (
         <div className={styles.login__form}>
             <h2>Log in</h2>
-            <form className={styles.form}>
+            <form onSubmit={handleFormSubmit} className={styles.form}>
                 <div className={styles.input__box}>
                     <label htmlFor='email'>E-mail</label>
-                    <input name='email' type="email" placeholder='yourEmail@gmail.com'/>
+                    <input
+                        name='email' 
+                        type="email" 
+                        placeholder='yourEmail@gmail.com'
+                        value={userLogin.email}
+                        onChange={handleEmailChange}
+                    />
                 </div>
                 <div className={styles.input__box}>
                     <label htmlFor='password'>Password</label>
-                    <input name='password' type="password" placeholder='********'/>
+                    <input 
+                        name='password' 
+                        type="password" 
+                        placeholder='********'
+                        value={userLogin.password}
+                        onChange={handlePasswordChange}
+                    />
                 </div>
                 <button className={styles.btn} type='submit'>Submit</button>
             </form>
