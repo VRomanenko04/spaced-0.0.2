5fea36018f980883a0e37abe8912eae3936a1de4
VRomanenko04 <vovasan2004@gmail.com>
2023-10-02 22:38:28 +0300

    Исправил ошибки импортов

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,14 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { IUser } from "store/userAuth/userAuth.slice";
-import { RootState } from "store/store";
+import { IUser } from "../store/userAuth/userAuth.slice";
+import { RootState } from "../store/store";
 
 export const useAuth = () => {
-    const { id, email, password }: IUser = useSelector((state: RootState) => state.userAuth);
+    const { id, email, token }: IUser = useSelector((state: RootState) => state.userAuth);
 
     return {
         isAuth: !!email,
         email,
-        password,
+        token,
         id
     }
 }
