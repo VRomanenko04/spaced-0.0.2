863de1a4508f79b4204bbf3334da6178b01bd308
VRomanenko04 <vovasan2004@gmail.com>
2023-10-16 23:58:16 +0300

    Сделал анимацию на header главной страницы

--- src/components/simple/HomeHeader/HomeHeader.tsx
+++ src/components/simple/HomeHeader/HomeHeader.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { motion } from "framer-motion";
 import styles from './HomeHeader.module.scss';
 import AuthPopup from '../../smart/AuthPopup/AuthPopup';
+
 
 const HomeHeader = () => {
     const [isActive, setIsActive] = useState(false)
     const [isChosed, setIsChosed] = useState('');
+
+    const variants = {
+        hidden: { background: 'rgba(0,0,0,1)' },
+        visible: { background: 'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,4,23,1) 38%, rgba(236,148,34,1) 80%, rgba(249,249,249,1) 99%)' }
+    }
 
     const handleLogin = () => {
         setIsChosed('LogIn');
@@ -18,6 +25,12 @@ const HomeHeader = () => {
 
     return (
         <header className={styles.header}>
+            <motion.div 
+                className={styles.background}
+                initial={{ x: "100%" }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1 }}>
+            </motion.div>
             <div className={styles.container}>
                 <h1 className={styles.top}>SPACEDEV</h1>
                 <p className={styles.text}>start your way in coding by playing</p>
