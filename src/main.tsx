2cfaef8e8afc67a033b32c2e94e759c3063ede73
VRomanenko04 <vovasan2004@gmail.com>
2023-10-22 23:46:07 +0300

    Изменение для работы с gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/_index.scss'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
@@ -10,9 +10,9 @@ import './firebase.ts'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/spacedev-0.0.2' : '/'}>
+      <HashRouter basename={process.env.NODE_ENV === 'production' ? '/spacedev-0.0.2' : '/'}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
