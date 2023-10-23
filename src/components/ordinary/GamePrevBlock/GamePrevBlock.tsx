9d5075407983144adfeb3b46c4b81dce94d5fbe7
VRomanenko04 <vovasan2004@gmail.com>
2023-10-23 20:53:28 +0300

    Убрал анимацию, которая вызывала ошибку

--- src/components/ordinary/GamePrevBlock/GamePrevBlock.tsx
+++ src/components/ordinary/GamePrevBlock/GamePrevBlock.tsx
@@ -1,60 +1,24 @@
-import { AnimationControls, motion, useAnimation } from 'framer-motion';
 import styles from './GamePrevBlock.module.scss';
 import asteroid1 from '../../../assets/imgs/planet1.webp';
 import asteroid2 from '../../../assets/imgs/planet2.webp';
 import asteroid3 from '../../../assets/imgs/planet3.webp';
-import { useEffect } from 'react';
 
-const animations = [
-    { x: 20, y: 55, style: styles.asteroid1, src: asteroid1, alt: 'asteroid1' },
-    { x: 40, y: 35, style: styles.asteroid2, src: asteroid2, alt: 'asteroid2' },
-    { x: -20, y: -10, style: styles.asteroid3, src: asteroid3, alt: 'asteroid3' },
+const asteroids = [
+    {style: styles.asteroid1, src: asteroid1, alt: 'asteroid1' },
+    {style: styles.asteroid2, src: asteroid2, alt: 'asteroid2' },
+    {style: styles.asteroid3, src: asteroid3, alt: 'asteroid3' },
 ];
 
 const GamePrevBlock = () => {
-    const controlsArray = animations.map(() => useAnimation());
-
-    const animate = async (controls: AnimationControls, index: number) => {
-        while (true) {
-            await controls.start({
-                x: animations[index].x,
-                y: animations[index].y,
-                rotate: 360,
-                transition: { duration: 12, ease: "linear" },
-            });
-            await controls.start({
-                x: 0,
-                y: 0,
-                rotate: 0,
-                transition: { duration: 12, ease: "linear" },
-            });
-        }
-    };
-
-    useEffect(() => {
-        const intervalIds = controlsArray.map((controls, index) => {
-            return setInterval(() => {
-                animate(controls, index);
-            }, 24000); // Период анимации
-        });
-
-        return () => {
-            intervalIds.forEach((intervalId) => {
-                clearInterval(intervalId);
-            });
-        };
-    }, [controlsArray]);
 
     return (
         <section className={styles.game_container}>
-            {controlsArray.map((controls, index) => (
+            {asteroids.map((_asteroid, index) => (
                 <div key={index}>
-                    <motion.img
-                        className={animations[index].style}
-                        src={animations[index].src}
-                        alt={animations[index].alt}
-                        initial={false}
-                        animate={controls}
+                    <img
+                        className={asteroids[index].style}
+                        src={asteroids[index].src}
+                        alt={asteroids[index].alt}
                     />
                 </div>
             ))}
