770ddd6c9912d30fa6474d4cacf6fde0ad271526
VRomanenko04 <vovasan2004@gmail.com>
2023-10-24 17:56:10 +0300

    Использовал хук useUsename в ещё одном компоненте

--- src/pages/Plans/PlansPage.tsx
+++ src/pages/Plans/PlansPage.tsx
@@ -1,12 +1,21 @@
+import { useDispatch } from "react-redux";
 import NavBar from "../../components/UI/NavBar/NavBar";
 import SubscribePlan from "../../components/smart/SubscribePlan/SubscribePlan";
 import styles from './PlansPage.module.scss';
+import { AppDispatch } from "../../store/store";
+import { useAuth } from "../../hooks/useAuth";
+import { useUsername } from "../../hooks/useUsername";
 
 const PlansPage = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const authUser = useAuth();
+    const username = useUsername(authUser, dispatch);
+
+
     return (
         <>
             <header>
-                <NavBar/>
+                <NavBar username={username}/>
             </header>
             <main className={styles.container}>
                 <div className={styles.plans_pos}>
